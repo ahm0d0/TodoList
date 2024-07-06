1b383c5a50f5f6e05acc8f9356a58d679eb46d90
Ahmedhassan <55562878+ahm0d0@users.noreply.github.com>
2024-07-06 18:25:30 +0300

    Update Colors.jsx

--- app/components/Colors.jsx
+++ app/components/Colors.jsx
@@ -13,13 +13,12 @@ export default function Colors() {
 
   return (
  
-    <div className="flex justify-center items-center">
+ <div className="flex justify-center items-center">
       <div onClick={handleGrayColor} className="flex justify-center rounded-xl bg-opacity-75 bg-gray-500 w-1/4 mx-1 h-20 md:h-24 items-center duration-700 hover:bg-opacity-100 hover:cursor-pointer hover:z-10  md:hover:w-96 absolute">
-          <Image className="mx-1" src="/old-man.png" alt="Example Image" width={60} height={60} />
-          <p>MarkUp</p>
+          <Image className="mx-1 " src="/old-man.png" alt="Example Image" width={60} height={60} />
+          <p className="hidden md:block">MarkUp</p>
         </div>
     </div>
-        
 
   
   );
